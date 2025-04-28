ee13778193f96dacf2dfffaf860c4ffe896a4e9f
ljyGmail <liangjy881@gmail.com>
2025-04-28 19:22:29 +0900

    084 辅助功能

---
+++ src/main/java/com/atguigu/imperial/court/util/ImperialCourtConst.java
@@ -0,0 +1,15 @@
+package com.atguigu.imperial.court.util;
+
+/**
+ * ClassName: ImperialCourtConst
+ * Package: com.atguigu.imperial.court.util
+ * Description:
+ *
+ * @Author ljy
+ * @Create 2025-04-28 오후 7:18
+ * @Version 1.0
+ */
+public class ImperialCourtConst {
+    public static final String LOGIN_FAILED_MESSAGE = "账号、密码错误，不可进宫！";
+    public static final String ACCESS_DENIED_MESSAGE = "宫闱禁地，不可擅入！";
+}
